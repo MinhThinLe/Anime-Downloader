e7b17fa291f3eedde33f70a58cd29ecd0ed1abc3
T0ast <minhthinle69@gmail.com>
2026-02-13 19:11:41 +0700

    Refactored path.rs

--- src/paths.rs
+++ src/paths.rs
@@ -12,80 +12,24 @@ const CONFIG_FILE_CONTENT: &str = include_str!("../examples/watchlist.toml");
 const ERROR_UNREADABLE_FILESYSTEM: &str =
     "Unable to read the system's file structure, maybe you've got a permission issue?";
 
-fn make_config_path() {
-    let config_path = get_config_path();
-    match fs::create_dir_all(&config_path) {
-        Ok(_) => (),
-        Err(error) => {
-            println!(
-                "Unable to mkdir {:?} due to {}, exiting now",
-                config_path, error
-            );
-            exit(1);
-        }
-    }
-}
-
-fn make_config_file() {
-    let config_file = get_config_file_path();
-    let mut config_file = match File::create(&config_file) {
-        Ok(config_file) => config_file,
-        Err(error) => {
-            println!(
-                "Unable to touch {:?} due to {}, exiting now",
-                config_file, error
-            );
-            exit(1);
-        }
-    };
-
-    if let Err(error) = config_file.write_all(CONFIG_FILE_CONTENT.as_bytes()) {
-        println!("Unable to create config file due to {error}");
-    }
-}
-
-fn make_state_path() {
-    let state_path = get_state_path();
-    match fs::create_dir_all(&state_path) {
-        Ok(_) => (),
-        Err(error) => {
-            println!(
-                "Unable to mkdir {:?} due to {}, exiting now",
-                state_path, error
-            );
-            exit(1);
-        }
-    }
-}
-
-fn make_state_file() {
-    let state_file_path = get_state_file_path();
-    match File::create(&state_file_path) {
-        Ok(_) => (),
-        Err(error) => {
-            println!(
-                "Unable to touch {:?} due to {}, exiting now",
-                state_file_path, error
-            );
-            exit(1);
-        }
-    }
-}
-
 pub fn make_config() {
     let Ok(config_path_exists) = exists(get_config_path()) else {
         println!("{}", ERROR_UNREADABLE_FILESYSTEM);
         exit(1);
     };
     if !config_path_exists {
-        make_config_path();
+        make_directory(&get_config_path());
     }
     let Ok(config_file_exists) = exists(get_config_file_path()) else {
         println!("{}", ERROR_UNREADABLE_FILESYSTEM);
         exit(1);
     };
     if !config_file_exists {
-        make_config_file();
+        let mut config_file = make_file(&get_config_file_path());
+
+        if let Err(error) = config_file.write_all(CONFIG_FILE_CONTENT.as_bytes()) {
+            println!("Unable to write to config file due to error {error}");
+        }
     }
 }
 
@@ -96,7 +40,7 @@ pub fn make_state() {
     };
 
     if !state_path_exists {
-        make_state_path();
+        make_directory(&get_state_path());
     }
 
     let Ok(state_file_exists) = exists(get_state_file_path()) else {
@@ -105,17 +49,27 @@ pub fn make_state() {
     };
 
     if !state_file_exists {
-        make_state_file();
+        make_file(&get_state_file_path());
     }
 }
 
 pub fn make_directory(directory: &Path) {
-    match fs::create_dir_all(directory) {
-        Ok(_) => (),
+    if let Err(error) = fs::create_dir_all(directory) {
+        println!(
+            "Unable to create directory {:?} due to {}, exiting now",
+            directory, error
+        );
+        exit(1);
+    }
+}
+
+pub fn make_file(file_path: &Path) -> File {
+    match File::create(&file_path) {
+        Ok(file) => file,
         Err(error) => {
             println!(
-                "Unable to create directory {:?} due to {}, exiting now",
-                directory, error
+                "Unable to touch {:?} due to {}, exiting now",
+                file_path, error
             );
             exit(1);
         }
@@ -147,11 +101,9 @@ pub fn get_state_path() -> PathBuf {
 }
 
 pub fn get_home_path() -> PathBuf {
-    match std::env::home_dir() {
-        Some(dir) => dir,
-        None => {
-            println!("Couldn't get the user's home directory, exiting");
-            exit(1);
-        }
-    }
+    let Some(dir) = std::env::home_dir() else {
+        println!("Couldn't get the user's home directory, exiting");
+        exit(1);
+    };
+    dir
 }
